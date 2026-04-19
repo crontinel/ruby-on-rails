d81d13a9d33180cd528850b6cb4c1e29efb21dca
Harun R Rayhan <harun.b13@gmail.com>
2026-04-19 17:09:20 +0600

    fix: add require railties before crontinel-rails so Engine can inherit ::Rails::Engine

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,6 +4,7 @@ require "minitest/autorun"
 require "minitest/reporters"
 require "active_support"
 require "active_support/ordered_options"
+require "railties"
 require "crontinel-rails"
 
 Minitest::Reporters.use!
