6b88c73f454ed44a8b0d8907198cbf8be039e42f
HarunRRayhan <harun.b13@gmail.com>
2026-04-19 11:09:55 +0600

    fix: add Rakefile and test_helper.rb (CI was failing because rake test had no task)

---
+++ test/test_helper.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require "minitest/autorun"
+require "minitest/reporters"
+require "active_support"
+require "active_support/ordered_options"
+require "crontinel-rails"
+
+Minitest::Reporters.use!
