684a22e29917a531636828442b8cb0a7bffdc32b
Harun R Rayhan <harun.b13@gmail.com>
2026-04-19 17:25:14 +0600

    fix: replace invalid generator_commands with initializer in Railtie

--- lib/crontinel/rails/railtie.rb
+++ lib/crontinel/rails/railtie.rb
@@ -5,10 +5,9 @@ module Crontinel
     class Railtie < ::Rails::Railtie
       config.crontinel = ActiveSupport::OrderedOptions.new
 
-      generator_commands :crontinel do
-        {
-          api_key: ENV["CRONTINEL_API_KEY"]
-        }
+      initializer "crontinel.configure" do |app|
+        api_key = app.config.crontinel.api_key || ENV["CRONTINEL_API_KEY"]
+        Crontinel.configure { |c| c.api_key = api_key } if api_key && defined?(Crontinel)
       end
 
       rake_tasks do
